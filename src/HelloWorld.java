675f285cf03a10c9814aba244d5fd962853972a2
srini <mesasri21@gmail.com>
2020-04-27 16:20:51 +0530

    Again I have altered the source

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -10,6 +10,13 @@ public class HelloWorld {
          System.out.println("Demo of Git for two diffent timezone user working on same file!");
   
       
+         System.out.println("Hello world!");
+         System.out.println("Im facing merge pull chanlleges from Git to Gitxx!");
+         System.out.println("Hey I had to delete the file it complianed while pulling in bin dir of Gitxx folder and then pull worked");
+         
+         
+         
+         
 	}
 
 }
